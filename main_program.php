2f4fed740e978a30172711e785cdd43b3bf7bc33
alan saji <115177536+alansaji-01@users.noreply.github.com>
2026-06-09 15:59:40 +0530

    Add files via upload

---
+++ main_program.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("math_utils.php");
+
+echo "Addition: " . add(15, 7) . "<br>";
+echo "Subtraction: " . subtract(15, 7) . "<br>";
+echo "Multiplication: " . multiply(15, 7) . "<br>";
+echo "Division: " . divide(15, 7) . "<br>";
+echo "Factorial of 5: " . factorial(5);
+
+?>
